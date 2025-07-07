1d7c837313462216cf63c1ae967e449017f803aa
Shreyas <shreyasks978@gmail.com>
2025-07-07 12:18:47 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3009;
+const port = 3207;
 
 // Database connection configuration
 const pool = new Pool({
@@ -208,5 +208,5 @@ app.delete('/api/offboarding', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://3.110.213.21:${port}`);
+    console.log(`Server running at http://3.85.61.23:${port}`);
 });
